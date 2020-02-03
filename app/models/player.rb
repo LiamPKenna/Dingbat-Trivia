68e6c4082455f133bd8c4e165d8db1fed200daf9
Student <example@example.com>
2020-02-03 10:33:42 -0800

    Add check_answer method to Player

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,17 @@
 class Player < ApplicationRecord
   belongs_to :room, dependent: :destroy
+
+  attr_accessor :current_answer
+
+  def check_answer(correct_answer)
+    if self.current_answer == correct_answer.to_i
+      self.score += 10
+      self.current_answer = 0
+      return true
+    else
+      self.current_answer = 0
+      return false
+    end
+  end
+
 end
