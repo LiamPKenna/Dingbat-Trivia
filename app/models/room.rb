6cc151a99a8f2541751b4e6e3abbeb94a664d9ef
Student <example@example.com>
2020-02-03 10:34:41 -0800

    Add methods to check player answers and move to the next question

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,19 @@
 class Room < ApplicationRecord
   has_many :players
+
+  def check_player_answers(correct_answer)
+    correct_answers = []
+    self.players.each do |player|
+      if player.check_answer(correct_answer)
+        correct_answers.push(player)
+      end
+    end
+    correct_answers
+  end
+
+  def next_question
+    q_arr = self.questions.split(',')
+    self.current_question = q_arr.shift.to_i
+    self.questions = q_arr.join(',')
+  end
 end
