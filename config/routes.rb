6350296efb2c36f5b5bae25bf038c9620118b206
LiamPKenna <liampkenna@gmail.com>
2020-02-03 15:27:21 -0800

    Add routes for creating players and rooms

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'rooms#new'
   get 'rooms/new'
   get 'rooms/join'
   get 'rooms/:room_id/players/:id', to: 'rooms#show', as: 'player'
-  get 'rooms/:room_id', to: 'rooms#host', as: 'room' 
-  root to: 'rooms#new'
+  get 'rooms/:room_id', to: 'rooms#host', as: 'room'
+  post 'rooms', to: 'rooms#create'
+  post 'join', to: 'rooms#new_player'
   mount ActionCable.server => '/cable'
 end
