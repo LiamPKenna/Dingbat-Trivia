74e3f335faa45f20d82958fc02c6d68cbc95f31f
Student <example@example.com>
2020-02-03 16:33:13 -0800

    Clean-up views scripts

--- app/assets/javascripts/views/player.js
+++ app/assets/javascripts/views/player.js
@@ -37,101 +37,92 @@ const playerViewHeader = (playerName) => {
         `
     );
 }
-const playerViewQuestion = (question, playerID) => {
+const playerViewQuestion = (questionOptions, playerID) => {
     return (
         `
         <main>
-            <style>
-                main {
-                    width: 80%;
-                    margin: 0 auto;
-                }
-            </style>
-            ${playerViewQuestionForm(question.options, playerID)}
+            <form id='question-form'>
+                <style>
+                    main {
+                        width: 80%;
+                        margin: 0 auto;
+                    }
+                    #input-options {
+                        display: flex;
+                        flex-direction: column;
+                    }
+                    .radio-button {
+                        margin: 1vh;
+                    }
+                    .radio-button p {
+                        height: 10vh;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        font-size: 3.5vh;
+                        border-radius: 10vh;
+
+                        border: 5px solid black;
+                        background-color: rgba(255, 255, 255, 0.8);
+                        color: black;
+                    }
+                    .radio-button input:checked + .option-text {
+                        border: 5px solid white;
+                        background-color: rgba(0, 0, 0, 0.6);
+                        color: white;
+                        animation: 1s selected linear infinite;
+                    }
+                    @keyframes selected {
+                        0%, 50%, 100% {
+                            box-shadow: 0 0 6px 6px black;
+                            transform: rotateZ(0deg)
+                        }
+                        25% {
+                            box-shadow: 0 0 3px 3px black;
+                            transform: rotateZ(-1.5deg);
+                        }
+                        75% {
+                            box-shadow: 0 0 3px 3px black;
+                            transform: rotateZ(1.5deg);
+                        }
+                    }
+
+                    .radio-button input {
+                        display: none;
+                    }
+
+                    #question-form button {
+                        height: 20vh;
+                        width: 20vh;
+                        font-size: 10vh;
+                        background-color: rgb(240, 40, 40);
+                        color: white;
+                        text-shadow: -3px -3px black, -3px 3px black, 3px -3px black, 3px 3px black;
+                        padding: 1vh;
+                        border-radius: 50%;
+                        position: absolute;
+                        bottom: 5vh;
+                        left: calc(50vw - 11vh);
+                        border: 5px solid white;
+                        box-shadow: 0 0 1px 5px black;
+                    }
+                </style>
+                <div id='input-options'>
+                    ${
+                        questionOptions.map(
+                            (option, index) => {
+                                return playerViewQuestionLabel(option, index, playerID);
+                            }
+                        ).join('\n')
+                    }
+                </div>
+                <button type='submit'>Go!</button>
+            </form>
         </main>
         `
     );
 }
-const playerViewQuestionForm = (questionOptions, playerID) => {
-    return (
-        `
-        <form id='question-form'>
-            <style>
-                #input-options {
-                    display: flex;
-                    flex-direction: column;
-                }
-                .radio-button {
-                    margin: 1vh;
-                }
-                .radio-button p {
-                    height: 10vh;
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                    font-size: 3.5vh;
-                    border-radius: 10vh;
-
-                    border: 5px solid black;
-                    background-color: rgba(255, 255, 255, 0.8);
-                    color: black;
-                }
-                .radio-button input:checked + .option-text {
-                    border: 5px solid white;
-                    background-color: rgba(0, 0, 0, 0.6);
-                    color: white;
-                    animation: 1s selected linear infinite;
-                }
-                @keyframes selected {
-                    0%, 50%, 100% {
-                        box-shadow: 0 0 6px 6px black;
-                        transform: rotateZ(0deg)
-                    }
-                    25% {
-                        box-shadow: 0 0 3px 3px black;
-                        transform: rotateZ(-1.5deg);
-                    }
-                    75% {
-                        box-shadow: 0 0 3px 3px black;
-                        transform: rotateZ(1.5deg);
-                    }
-                }
-
-                .radio-button input {
-                    display: none;
-                }
-
-                #question-form button {
-                    height: 20vh;
-                    width: 20vh;
-                    font-size: 10vh;
-                    background-color: rgb(240, 40, 40);
-                    color: white;
-                    text-shadow: -3px -3px black, -3px 3px black, 3px -3px black, 3px 3px black;
-                    padding: 1vh;
-                    border-radius: 50%;
-                    position: absolute;
-                    bottom: 5vh;
-                    left: calc(50vw - 11vh);
-                    border: 5px solid white;
-                    box-shadow: 0 0 1px 5px black;
-                }
-            </style>
-            <div id='input-options'>
-                ${
-                    questionOptions.map(
-                        (option, index) => {
-                            return playerViewQuestionFormLabel(option, index, playerID);
-                        }
-                    ).join('\n')
-                }
-            </div>
-            <button type='submit'>Go!</button>
-        </form>
-        `
-    );
-}
-const playerViewQuestionFormLabel = (text, index, playerID) => {
+const playerViewQuestionLabel = (text, index, playerID) => {
     return (
         `
         <label class='radio-button'>
@@ -156,7 +147,7 @@ const playerView = (playerName, playerID, question) => {
             }
         </style>
         ${playerViewHeader(playerName)}
-        ${playerViewQuestion(question, playerID)}
+        ${playerViewQuestion(question.options, playerID)}
          `
     );
 }
